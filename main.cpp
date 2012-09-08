979a4295518493a6eb479b915669862bb854e206
Kohe Tokoi <tokoik@gmail.com>
2012-09-09 02:26:02 +0900

    add error handling

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
+#include <iostream>
+#include <cstdlib>
 #include <GL/glfw.h>
 
 int main(int argc, const char * argv[])
 {
-  glfwInit();
+  // GLFW を初期化する
+  if (!glfwInit())
+  {
+    // 初期化に失敗した
+    std::cerr << "Can't initialize GLFW." << std::endl;
+    exit(EXIT_FAILURE);
+  }
 
-  glfwOpenWindow(0, 0, 0, 0, 0, 0, 0, 0, GLFW_WINDOW);
+  // GLFW のウィンドウを開く
+  if (!glfwOpenWindow(0, 0, 0, 0, 0, 0, 0, 0, GLFW_WINDOW))
+  {
+    // ウィンドウが開けなかった
+    std::cerr << "Can't open GLFW window." << std::endl;
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
 
+  // 図形を表示する
   while (glfwGetWindowParam(GLFW_OPENED))
   {
     /*
@@ -15,7 +31,8 @@ int main(int argc, const char * argv[])
     glfwSwapBuffers();
   }
 
+  // GLFW を終了する
   glfwTerminate();
 
-  return 0;
+  return EXIT_SUCCESS;
 }
