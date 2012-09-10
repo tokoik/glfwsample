f9b3e668b76cc29589413943b4b62539a6472061
Kohe Tokoi <tokoik@gmail.com>
2012-09-10 22:03:30 +0900

    add shader

--- main.cpp
+++ main.cpp
@@ -55,11 +55,61 @@ int main(int argc, const char * argv[])
   // OpenGL の初期設定
   init();
 
+  // バーテックスシェーダのソースプログラム
+  static const GLchar *vsrc[] =
+  {
+    "#version 150 core\n",
+    "in vec4 pv;",
+    "void main(void)",
+    "{",
+    "  gl_Position = pv;",
+    "}",
+  };
+
+  // コンパイル／リンク結果
+  GLint status;
+
+  // バーテックスシェーダのシェーダオブジェクト
+  GLuint vobj = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vobj, sizeof vsrc / sizeof vsrc[0], vsrc, NULL);
+  glCompileShader(vobj);
+
+  // フラグメントシェーダのソースプログラムの作成
+  static const GLchar *fsrc[] =
+  {
+    "#version 150 core\n",
+    "out vec4 fc;",
+    "void main(void)",
+    "{",
+    "  fc = vec4(1.0, 0.0, 0.0, 0.0);",
+    "}",
+  };
+
+  // フラグメントシェーダのシェーダオブジェクトの作成
+  GLuint fobj = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fobj, sizeof fsrc / sizeof fsrc[0], fsrc, NULL);
+  glCompileShader(fobj);
+
+  // プログラムオブジェクトの作成
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vobj);
+  glDeleteShader(vobj);
+  glAttachShader(program, fobj);
+  glDeleteShader(fobj);
+
+  // プログラムオブジェクトのリンク
+  glBindAttribLocation(program, 1, "pv");
+  glBindFragDataLocation(program, 0, "fc");
+  glLinkProgram(program);
+
   // 図形を表示する
   while (glfwGetWindowParam(GLFW_OPENED))
   {
     // 画面消去
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // シェーダプログラムの使用
+    glUseProgram(program);
 
     /*
     ** ここで OpenGL による描画を行う
